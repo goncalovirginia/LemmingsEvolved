5121c8f1d2278b5738fe74b37be564cb45c0a07a
xXParagoNXx <gonvirginia.blueslime@gmail.com>
2022-03-30 19:54:55 +0100

    replaced trial class with simple solve method in Main

--- src/Main.java
+++ src/Main.java
@@ -5,30 +5,60 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		TurboScanner in = new TurboScanner(System.in);
 		int numTrials = in.nextInt();
+		long[][] trialSolutions = new long[numTrials][2];
 		
 		for (int t = 0; t < numTrials; t++) {
 			int numLemmings1 = in.nextInt();
 			char[] tribes1 = new char[numLemmings1+1];
-			int[] points1 = new int[numLemmings1+1];
+			long[] points1 = new long[numLemmings1+1];
 			
 			readSequence(in, numLemmings1, tribes1, points1);
 			
 			int numLemmings2 = in.nextInt();
 			char[] tribes2 = new char[numLemmings2+1];
-			int[] points2 = new int[numLemmings2+1];
+			long[] points2 = new long[numLemmings2+1];
 			
 			readSequence(in, numLemmings2, tribes2, points2);
 			
-			int[] result = (new Trial(tribes1, points1, tribes2, points2)).solve();
-			System.out.printf("%d %d\n", result[0], result[1]);
+			trialSolutions[t] = solve(tribes1, points1, tribes2, points2);
+		}
+		
+		for (long[] trialSolution : trialSolutions) {
+			System.out.printf("%d %d\n", trialSolution[0], trialSolution[1]);
 		}
 	}
 	
-	private static void readSequence(TurboScanner in, int numLemmings, char[] tribes, int[] points) throws IOException {
+	private static void readSequence(TurboScanner in, int numLemmings, char[] tribes, long[] points) throws IOException {
 		for (int l = 1; l <= numLemmings; l++) {
 			tribes[l] = in.nextChar(); in.nextChar();
 			points[l] = in.nextInt();
 		}
+	}
+	
+	private static long[] solve(char[] tribes1, long[] points1, char[] tribes2, long[] points2) {
+		long[][] maxPoints = new long[points1.length][points2.length], minPairs = new long[points1.length][points2.length];
+		
+		for (int i = 1; i < points1.length; i++) {
+			for (int j = 1; j < points2.length; j++) {
+				maxPoints[i][j] = maxPoints[i-1][j-1];
+				minPairs[i][j] = minPairs[i-1][j-1];
+				
+				if (tribes1[i] == tribes2[j]) {
+					maxPoints[i][j] += points1[i] + points2[j];
+					minPairs[i][j] += 1;
+				}
+				if (maxPoints[i-1][j] > maxPoints[i][j]) {
+					maxPoints[i][j] = maxPoints[i-1][j];
+					minPairs[i][j] = minPairs[i-1][j];
+				}
+				if (maxPoints[i][j-1] > maxPoints[i][j]) {
+					maxPoints[i][j] = maxPoints[i][j-1];
+					minPairs[i][j] = minPairs[i][j-1];
+				}
+			}
+		}
+		
+		return new long[] {maxPoints[points1.length-1][points2.length-1], minPairs[points1.length-1][points2.length-1]};
 	}
 	
 }
